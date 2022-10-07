691e9349291b6f2d14eafea9acd1e58d5356aef6
Ignatev Nikita <ignatev@medilon.ru>
2022-10-07 12:31:46 +0300

    Add docstring and fix template bugs

--- flform.py
+++ flform.py
@@ -1,3 +1,6 @@
+"""
+Test submodule working for Jenkins CI/CD
+"""
 from flask import Blueprint,render_template
 
 flform = Blueprint('flokoform', __name__, template_folder='templates', static_folder='static')
@@ -5,4 +8,7 @@ flform = Blueprint('flokoform', __name__, template_folder='templates', static_fo
 
 @flform.route('/')
 def index():
+    """
+    Test index page for submodule
+    """
     return render_template('flform/contact.html', title='Форма обратной связи')
