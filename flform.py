11d0db99a0628f102dc76e7424adf107691e0cd3
Ignatev Nikita <ignatev@medilon.ru>
2022-10-07 11:51:27 +0300

    Add contact form

--- flform.py
+++ flform.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 flform = Blueprint('flokoform', __name__, template_folder='templates', static_folder='static')
 
 
 @flform.route('/')
 def index():
-    return 'flokoform'
+    return render_template('flform/contact.html', title='Форма обратной связи')
