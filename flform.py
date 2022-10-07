e2bec8448b293d272711f2e47f6f1a11529b1595
Ignatev Nikita <ignatev@medilon.ru>
2022-10-07 09:20:39 +0300

    Add index page for module

--- flform.py
+++ flform.py
@@ -2,3 +2,6 @@ from flask import Blueprint
 
 flform = Blueprint('flokoform', __name__, template_folder='templates', static_folder='static')
 
+@flform.route('/')
+def index():
+    return: 'flokoform'
