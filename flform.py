61ac28f885285bed3f2b37f1a511ddd267e4ea9d
Ignatev Nikita <ignatev@medilon.ru>
2022-10-07 09:55:06 +0300

    Fix return for index page

--- flform.py
+++ flform.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 flform = Blueprint('flokoform', __name__, template_folder='templates', static_folder='static')
 
+
 @flform.route('/')
 def index():
-    return: 'flokoform'
+    return 'flokoform'
