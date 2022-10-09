5c64af81be999beae36271bce0a468bc585ee98c
Ignatev Nikita <ignatev@medilon.ru>
2022-10-09 23:11:00 +0300

    Add flash message to form

--- flform.py
+++ flform.py
@@ -1,14 +1,19 @@
 """
 Test submodule working for Jenkins CI/CD
 """
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 flform = Blueprint('flokoform', __name__, template_folder='templates', static_folder='static')
 
 
-@flform.route('/')
+@flform.route('/', methods=["POST"])
 def index():
     """
     Test index page for submodule
     """
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено')
+        else:
+            flash('Ошибка отправки')
     return render_template('flform/contact.html', title='Форма обратной связи')
